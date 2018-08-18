339b6912a7acb30f54d86ffaa1b0fe01a878bbd1
CMohamed <chadad.mohamed@gmail.com>
2018-08-18 04:13:03 +0100

    Commit using webStorm

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "500px";
+    document.getElementById("main").style.marginLeft = "500px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+
+// Condtions add and remove part
+var idcond=1;
+
+$(document).on('click', '#add', function(){
+    idcond++;
+    var html = '<div id=divCond'+idcond+'> <h6>Condition '+idcond+' : </h6> <input id=condition'+idcond.toString()+' type="text" name="myInput"></div>';
+    $('#conditions').append(html);
+});
+
+$(document).on('click', '#remove', function(){
+    var d ="#divCond"+idcond;
+    $(d).remove();
+    if (idcond>1 ) idcond--;
+});
+
+//collapse
+
 function expandAll(){
     $(".collapsible-header").addClass("active");
     $(".collapsible").collapsible({accordion: false});
@@ -10,3 +42,4 @@ function collapseAll(){
     $(".collapsible").collapsible({accordion: true});
     $(".collapsible").collapsible({accordion: false});
 }
+
